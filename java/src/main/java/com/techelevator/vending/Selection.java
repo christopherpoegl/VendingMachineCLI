54e3d4f4c92a9f7b548559b770b2e96e3f40c72f
David-S-Evans <84528162+David-S-Evans@users.noreply.github.com>
2021-07-24 10:33:51 -0400

    next

---
+++ java/src/main/java/com/techelevator/vending/Selection.java
@@ -0,0 +1,32 @@
+package com.techelevator.vending;
+
+public class Selection {
+    Stocker stocker = new Stocker();
+    String userInput;
+    public void createList(){
+        stocker.createList();
+    }
+    public void findItems(){
+        for(int i = 0; i < stocker.items.size(); i++){
+            Item selected = stocker.items.get(i);
+            if(selected.getPosition().equals("A2")){
+                Integer stockRemaining = Integer.parseInt(selected.getStock());
+                if(stockRemaining>0) {
+                    stockRemaining--;
+                    selected.setStock(stockRemaining.toString());
+                    System.out.println(selected.getItemInfoToString());
+                }
+                else System.out.println(selected.getName() + "-$" + selected.getPrice() + "== Stock Remaining:" + selected.getStock());
+            }
+        }
+    }
+    public void getItemList(){
+        for(Item tempList : stocker.items)
+            System.out.println(tempList.getItemInfoToString());
+    }
+    public void setUserInput(String userInput) {
+        this.userInput = userInput;
+    }
+
+
+}
