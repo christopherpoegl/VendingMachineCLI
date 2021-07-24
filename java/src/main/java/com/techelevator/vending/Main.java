816c49a66f60204f4aec10f0cd7c81bb3fb425f6
Josh McCray <joshjmccray@gmail.com>
2021-07-24 12:10:21 -0400

    fixing merge conflicts

--- java/src/main/java/com/techelevator/vending/Main.java
+++ java/src/main/java/com/techelevator/vending/Main.java
@@ -1,26 +1,10 @@
 package com.techelevator.vending;
-
-import com.techelevator.view.Purchase;
-
 public class Main {
-<<<<<<< HEAD
-    public static void main(String[] args){
-        Purchase moneyCalc = new Purchase();
-        moneyCalc.addMoney(525);
-        moneyCalc.withdrawMoney(105);
-        moneyCalc.changeBack();
-
-=======
-
     public static void main(String[] args){
         Selection selection = new Selection();
         selection.createList();
         selection.getItemList();
         System.out.println("----------------------");
         selection.findItems();
->>>>>>> cd16868e8cd114982f41c101ac6e48d7c1559c43
-
-
-
     }
 }
